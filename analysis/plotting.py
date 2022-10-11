f3db92b5baeaa30e6a4fdab412f2d91c59eb55c7
Ti-Fen Pan <tpan@moveworks.ai>
2022-10-10 20:32:35 -0700

    all block series

--- analysis/plotting.py
+++ analysis/plotting.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+import mne
 import matplotlib.pyplot as plt
 
+from biomarkers import (
+    EEG_CHANEL_NAMES,
+    EEG_NUM_CHANNELS,
+    EEG_MONTAGES,
+)
 from calculate_correlation import (
     EEG_BANDS_NAMES,
+    EEG_BANDS_LIST,
     get_pearson_corr_with_stats_features,
     get_eeg_spectral_pearson_correlation,
     get_eeg_features_means,
 )
-from feature_extraction import EEG_BANDS
+from feature_extraction import EEG_BANDS, Feature
 
 
 def get_pearson_correlation_series(
@@ -29,19 +36,21 @@ def get_pearson_correlation_series(
     return ser_list
 
 
-def get_eeg_pearson_correlation_series(
-    all_block: np.ndarray, labels: list, num_channel: int = 128
+def get_eeg_pearson_correlation_series_by_block(
+    feature_to_pc: np.ndarray,
+    all_block_names: list,
+    block_num: int = 0,
+    num_channel: int = 128,
 ):
     ser_list = []
+    block_name = all_block_names[block_num]
     index = list(range(1, num_channel + 1, 1))
-    for f in list(EEG_BANDS.keys()):
-        pearson_corr = get_eeg_spectral_pearson_correlation(all_block, labels, f)
+    for f in EEG_BANDS_LIST:
+        pearson_corr = feature_to_pc[f][:, block_num]
         ser = pd.Series(data=pearson_corr, index=index)
-        print(f"The mean {ser.mean()} over feature {f.name}")
-
         ser_list.append(
             {
-                "marker_name": f"EEG {f.name}",
+                "marker_name": f"EEG {f.name} at block {block_name}",
                 "value": pd.concat([ser.nlargest(5), ser.nsmallest(5)]),
             }
         )
@@ -60,7 +69,7 @@ def plot_series(nrow: int, ncol: int, ser_list):
         for c in range(ncol):
             marker_name = ser_list[count]["marker_name"]
             ser_list[count]["value"].plot(
-                ax=axes[r][c], kind="barh", title=f"{marker_name} Spearmanr Correlation"
+                ax=axes[r][c], kind="barh", title=f"{marker_name} Pearson Correlation"
             )
             count += 1
 
@@ -90,3 +99,44 @@ def plot_pearson_correlation_table(
     table.set_fontsize(8.5)
 
     plt.show()
+
+
+def plot_eeg_topomap(
+    label: str,
+    spectral_feature: Feature,
+    feature_to_pc: dict,
+    all_block_names: list,
+    num_epochs: int = 13,
+):
+    fig, axes = plt.subplots(
+        1,
+        11,
+        figsize=(30, 5),
+        gridspec_kw={"width_ratios": [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1]},
+    )
+
+    for i in range(len(all_block_names)):
+        axes[i].set_xlabel(all_block_names[i], fontsize=25)
+        axes[i].xaxis.set_label_position("top")
+
+    sampling_freq = 1  # in Hertz
+    info = mne.create_info(
+        ch_names=EEG_CHANEL_NAMES,
+        sfreq=sampling_freq,
+        ch_types=["eeg"] * EEG_NUM_CHANNELS,
+    )
+    info.set_montage(EEG_MONTAGES)
+
+    fig.suptitle(
+        f"{spectral_feature.name} <> {label} Pearson Correlation", fontsize=40, y=1.3
+    )
+    evoked_array = mne.EvokedArray(
+        feature_to_pc[spectral_feature],
+        info,
+        tmin=1,
+        nave=num_epochs,
+        comment="simulated",
+    )
+    return evoked_array.plot_topomap(
+        axes=axes, time_format="", ch_type="eeg", units="score", scalings=1
+    )
